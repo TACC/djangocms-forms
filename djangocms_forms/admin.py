fd7c7a33309c5b7cc380f392ec3344851c606bc2
Jake Rosenberg <jrosenberg@MacBook-Pro.lan>
2023-09-21 10:57:04 -0500

    replace deprecated is_ajax calls

--- djangocms_forms/admin.py
+++ djangocms_forms/admin.py
@@ -164,7 +164,7 @@ class FormSubmissionAdmin(admin.ModelAdmin):
                 message = _('No matching %s found for the given criteria. '
                             'Please try again.') % self.opts.verbose_name_plural
                 self.message_user(request, message, level=messages.WARNING)
-                if request.is_ajax():
+                if request.headers.get('x-requested-with') == 'XMLHttpRequest':
                     data = {
                         'reloadBrowser': True,
                         'submissionCount': 0,
--- djangocms_forms/views.py
+++ djangocms_forms/views.py
@@ -38,7 +38,7 @@ class FormSubmission(FormView):
         form.save(request=self.request)
         form_submission.send(sender=self.__class__, form=form.form_definition, cleaned_data=form.cleaned_data)
 
-        if self.request.is_ajax():
+        if self.request.headers.get('x-requested-with') == 'XMLHttpRequest':
             response = {
                 "formIsValid": True,
                 "redirectUrl": form.redirect_url,
@@ -61,7 +61,7 @@ class FormSubmission(FormView):
             return redirect("/")
 
     def form_invalid(self, form, *args, **kwargs):
-        if self.request.is_ajax():
+        if self.request.headers.get('x-requested-with') == 'XMLHttpRequest':
             response = {
                 "formIsValid": False,
                 "errors": form.errors,
